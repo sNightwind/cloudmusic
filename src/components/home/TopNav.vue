4c76a35692ac6be5614f21df242b0614f6a8aece
ShineWind <85597804+sNightwind@users.noreply.github.com>
2022-07-16 20:30:50 +0800

    Roure (#1)
    * 11
    
    * test
    
    * Delete test.js
    
    Co-authored-by: RoureUnder <Longjunyuan07@gmail.com>
    Co-authored-by: LongjunYuan <80234931+RoureUnder@users.noreply.github.com>

---
+++ src/components/home/TopNav.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="topNav">
+        <div class="topLeft">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-31liebiao"></use>
+            </svg>
+        </div>
+        <div class="topContent">
+            <span>我的</span>
+            <span class="active">发现</span>
+            <span>云村</span>
+            <span>视频</span>
+        </div>
+        <div class="topRight">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-sousuo"></use>
+            </svg>
+        </div>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="success">成功按钮</van-button>
+        <van-button type="default">默认按钮</van-button>
+        <van-button type="warning">警告按钮</van-button>
+        <van-button type="danger">危险按钮</van-button>
+
+    </div>
+</template>
+<style lang="less" scoped>
+.topNav {
+    width: 100%;
+    height: 1rem;
+    padding: .2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .topContent {
+        width: 65%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        // align-items: center;
+        font-size: .36rem;
+
+        .active {
+            font-weight: 900;
+        }
+    }
+}
+</style>
