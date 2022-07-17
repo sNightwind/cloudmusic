b35ab467caf3eb90af4a8c836324569e82479d11
sNightwind <yyuu0218@gmail.com>
2022-07-17 14:43:05 +0800

    hot fix

--- src/request/api/item.js
+++ src/request/api/item.js
@@ -1,17 +1,17 @@
 import service from "..";
 
-
 //获取歌曲的歌词/lyric?id=33894312
-export function getMusicLyric(data){
-    return service({
-        method:"GET",
-        url:'/lyric?id=${data}'
+export function getMusicLyric(data) {
+  return service({
+    method: "GET",
+    url: "/lyric?id=${data}",
+  });
+}
 
 //获取歌单详情页数据
-export function getMusicItemList(data){
-    return service({
-        method:"GET",
-        url:`/playlist/detail?id=${data}`
-
-    })
+export function getMusicItemList(data) {
+  return service({
+    method: "GET",
+    url: `/playlist/detail?id=${data}`,
+  });
 }
