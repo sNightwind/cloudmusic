bfdabc87423629807f9ae3775e87dc9d1c65aff9
sNightwind <yyuu0218@gmail.com>
2022-07-17 10:55:25 +0800

    实现底部播放栏（测试阶段

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    playList: [
+      {
+        al: {
+          //底部测试数据
+          id: 120577126,
+          name: "测试歌曲",
+          pic: 109951165929038820,
+          picUrl: "",
+          pic_str: "109951165929038814",
+        },
+        id: 1805395376,
+      },
+    ],
+    playListIndex: 0, //默认播放器
+    isBtnShow: true, //显示暂停按钮 / 切换播放按钮
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    updateIsBtnShow: function (state, value) {
+      state.isBtnShow = value;
+    },
+    updatePlayList: function (state, value) {
+      state.playList = value;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
